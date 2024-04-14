77a2eba3888bf72320e032791c8011bf7e70127f
KUSHANK PATIL <patilkushank@gmail.com>
2024-04-14 09:38:39 +0530

    Sidebar,NavBar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,14 +1,10 @@
-import{UserButton} from "@clerk/nextjs"
-
 const DashboardPage = ()=>{
   return (
     <div>
        <h2>Dashboardpage(Protected)</h2>
-       <UserButton afterSignOutUrl="/"/>
+
     </div>
-    // <Button variant="destructive">Click me</Button>
-   
-    // <h2 className="text-6xl text-yellow-400">HEllo Ai- Saas</h2>
+
   );
 }
 export default DashboardPage;
